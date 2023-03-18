d0ca8a29580ca1eb529607e660963af271ce8321
Andrea Cioni <supercionci@hotmail.it>
2023-03-18 22:28:34 +0100

    versioning script and values

---
+++ version.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "Check if checkout on master"
+CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+echo "Current branch is: $CURRENT_BRANCH"
+
+if [ $CURRENT_BRANCH != "master" ]; then
+    echo "Not checked out master, aborting..."
+    exit 1
+fi
+
+echo "Creating new version"
+
+FILE=pyproject.toml
+CURRENT_VERSION=$(cat $FILE | grep version | awk '{print $3}' | cut -d '"' -f 2)
+TO_UPDATE=(
+    pyproject.toml
+    chart/Chart.yaml
+    .github/workflows/main.yml
+)
+
+echo "Current version is: $CURRENT_VERSION. Enter new version:"
+
+read NEW_VERSION
+
+echo "New version is: $NEW_VERSION"
+
+for file in "${TO_UPDATE[@]}"
+do
+    echo "Patching $file ..."
+    sed -i '' "s/$CURRENT_VERSION/$NEW_VERSION/g" $file
+    git add $file
+done
+
+git commit -m "Releasing v$NEW_VERSION"
+
+git push
+
+git tag -a v$NEW_VERSION -m "Release v$NEW_VERSION"
+
+git push origin v$NEW_VERSION
+
+echo "Creating version for PyPi"
+
+python3 setup.py sdist bdist_wheel
+
+echo "Uploading to PyPi"
+
+python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
